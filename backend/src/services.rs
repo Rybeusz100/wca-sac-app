e0294fd2c615e11f9791d290ac320318f1884d19
Rybeusz100 <janzych@protonmail.com>
2024-06-27 18:00:55 +0200

    use NamedFile

--- backend/src/services.rs
+++ backend/src/services.rs
@@ -1,18 +1,19 @@
-use actix_web::{get, web, HttpResponse, Responder};
-use tokio::{fs::File, io::AsyncReadExt};
+use actix_files::NamedFile;
+use actix_web::{get, web, Responder};
+use log::info;
 
 use crate::wca_sac::generate_graph;
 
 #[get("/graph/{graph_type}")]
 async fn get_graph(path: web::Path<String>) -> impl Responder {
     let graph_type = path.into_inner();
+    let file_path = format!("../WCA_SAC/SAC_graph_{}.png", graph_type);
 
-    generate_graph(&graph_type).await.unwrap();
-
-    let mut file = File::open(format!("../WCA_SAC/SAC_graph_{}.png", graph_type))
-        .await
-        .unwrap();
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).await.unwrap();
-    HttpResponse::Ok().content_type("image/png").body(buffer)
+    if let Ok(file) = NamedFile::open_async(&file_path).await {
+        Ok(file)
+    } else {
+        info!("Generating graph for {}", graph_type);
+        generate_graph(&graph_type).await.unwrap();
+        NamedFile::open_async(file_path).await
+    }
 }
