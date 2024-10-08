5bb28c94c157569f3b2431d05a79d670a14af19f
Rybeusz100 <janzych@protonmail.com>
2024-10-08 21:31:28 +0200

    set User-Agent to fix export downloading

--- backend/src/wca_export.rs
+++ backend/src/wca_export.rs
@@ -6,17 +6,19 @@ use log::info;
 pub async fn download_and_unzip(target_dir: &str) -> anyhow::Result<()> {
     info!("Downloading WCA export");
     let zip_bytes = download().await?;
+    info!("Downloaded WCA export");
 
     info!("Extracting WCA export");
     zip_extract::extract(Cursor::new(zip_bytes), &PathBuf::from(target_dir), true)?;
+    info!("Extracted WCA export");
 
-    info!("Downloaded and extracted WCA export");
     Ok(())
 }
 
 async fn download() -> anyhow::Result<Vec<u8>> {
+    let client = reqwest::Client::new();
     let url = "https://www.worldcubeassociation.org/export/results/WCA_export.tsv";
-    let mut response = reqwest::get(url).await?;
+    let mut response = client.get(url).header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36").send().await?;
 
     if response.status().is_success() {
         let mut bytes = Vec::new();
